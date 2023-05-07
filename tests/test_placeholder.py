7a48f996b68a40fff6716199807bd9b5ccfe775b
Jakob Jul Elben <elbenjakobjul@gmail.com>
2023-05-07 17:57:36 +0200

    Fix error and reduce retry

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -14,7 +14,7 @@ def test_integration():
 
 
 @pytest.mark.e2e
-@tenacity.retry(stop=tenacity.stop_after_attempt(60 * 5), wait=tenacity.wait_fixed(1))
+@tenacity.retry(stop=tenacity.stop_after_attempt(60 * 1), wait=tenacity.wait_fixed(1))
 def test_e2e():
     try:
         httpx.get("http://localhost").status_code == 404
